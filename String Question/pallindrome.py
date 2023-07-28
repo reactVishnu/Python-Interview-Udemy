21387b572eb58f5d83613afedbfebb47b5cc8d4a
reactVishnu <134265914+reactVishnu@users.noreply.github.com>
2023-07-28 17:51:52 +0530

    Delete String Question directory

--- String Question/pallindrome.py
+++
@@ -1,9 +0,0 @@
-
-def pallindrome(stringular):
-    n = ''.join(filter(str.isalnum, stringular.lower()))
-    k = n == n[::-1]
-    return k
-
-
-s = 'A man, a plan, a canal: Panama'
-print(pallindrome(s))
--- String Question/reverse_input.py
+++
@@ -1,8 +0,0 @@
-def reverse_in_place(some_stirng):
-    if isinstance(some_stirng, float) or isinstance(some_stirng, list):
-        raise TypeError("Input must be a string or an integer")
-    x = type(some_stirng)
-    return x(str(some_stirng)[::-1])
-
-
-print(type(reverse_in_place(12345)))
